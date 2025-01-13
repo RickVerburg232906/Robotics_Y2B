0fc2aff2019ef44dd902bf76b2ee7cdce7dbd8d0
RickVerburg232906 <144121724+RickVerburg232906@users.noreply.github.com>
2025-01-13 16:45:57 +0100

    Update remote_training.py

--- remote_training.py
+++ remote_training.py
@@ -61,9 +61,6 @@ wandb_callback = WandbCallback(model_save_freq=1000,
 
 # variable for how often to save the model
 time_steps = 5000000
-for i in range(10):
-    # add the reset_num_timesteps=False argument to the learn function to prevent the model from resetting the timestep counter
-    # add the tb_log_name argument to the learn function to log the tensorboard data to the correct folder
-    model.learn(total_timesteps=time_steps, callback=wandb_callback, progress_bar=True, reset_num_timesteps=False,tb_log_name=f"runs/{run.id}")
-    # save the model to the models folder with the run id and the current timestep
-    model.save(f"models/{run.id}/{time_steps*(i+1)}")
+model.learn(total_timesteps=time_steps, callback=wandb_callback, progress_bar=True, reset_num_timesteps=False,tb_log_name=f"runs/{run.id}")
+# save the model to the models folder with the run id and the current timestep
+model.save(f"Robotics/models/{run.id}")
